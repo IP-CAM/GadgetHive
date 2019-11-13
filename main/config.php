0827e0e7dfe19c2fd2619e856a717c164a43e369
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2019-11-13 20:02:09 +0600

    Correction Config of main system
    Correction Config of main system

--- main/config.php
+++ main/config.php
@@ -9,7 +9,7 @@ define('HTTPS_SERVER', 'http://localhost/GadgetHive/main/');
 define('DIR_APPLICATION', 'C:/xampp/htdocs/GadgetHive/main/catalog/');
 define('DIR_SYSTEM', 'C:/xampp/htdocs/GadgetHive/main/system/');
 define('DIR_IMAGE', 'C:/xampp/htdocs/GadgetHive/main/image/');
-define('DIR_STORAGE', DIR_SYSTEM . 'C:/xampp/storage/');
+define('DIR_STORAGE', 'C:/xampp/storage/');
 define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
 define('DIR_TEMPLATE', DIR_APPLICATION . 'view/theme/');
 define('DIR_CONFIG', DIR_SYSTEM . 'config/');
