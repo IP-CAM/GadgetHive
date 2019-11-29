02819f33a5c8d744012066652b8bebfbfffd5093
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2019-11-29 20:12:33 +0600

    Server Version File extraced

---
+++ LiveServer/gadgethive.shubho.info/admin/config.php
@@ -0,0 +1,36 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'https://gadgethive.shubho.info/admin/');
+define('HTTP_CATALOG', 'https://gadgethive.shubho.info/');
+
+// HTTPS
+define('HTTPS_SERVER', 'https://gadgethive.shubho.info/admin/');
+define('HTTPS_CATALOG', 'https://gadgethive.shubho.info/');
+
+// DIR
+define('DIR_APPLICATION', '/home/shubhoinfo/gadgethive.shubho.info/admin/');
+define('DIR_SYSTEM', '/home/shubhoinfo/gadgethive.shubho.info/system/');
+define('DIR_IMAGE', '/home/shubhoinfo/gadgethive.shubho.info/image/');
+define('DIR_STORAGE', '/home/shubhoinfo/ocartdata/storage/');
+define('DIR_CATALOG', '/home/shubhoinfo/gadgethive.shubho.info/catalog/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/template/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_MODIFICATION', DIR_STORAGE . 'modification/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'shubhoinfo_oc790');
+define('DB_PASSWORD', 'SAS@8pF(46');
+define('DB_DATABASE', 'shubhoinfo_oc790');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oclo_');
+
+// OpenCart API
+define('OPENCART_SERVER', 'https://www.opencart.com/');
---
+++ LiveServer/gadgethive.shubho.info/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'https://gadgethive.shubho.info/');
+
+// HTTPS
+define('HTTPS_SERVER', 'https://gadgethive.shubho.info/');
+
+// DIR
+define('DIR_APPLICATION', '/home/shubhoinfo/gadgethive.shubho.info/catalog/');
+define('DIR_SYSTEM', '/home/shubhoinfo/gadgethive.shubho.info/system/');
+define('DIR_IMAGE', '/home/shubhoinfo/gadgethive.shubho.info/image/');
+define('DIR_STORAGE', '/home/shubhoinfo/ocartdata/storage/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/theme/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_MODIFICATION', DIR_STORAGE . 'modification/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'shubhoinfo_oc790');
+define('DB_PASSWORD', 'SAS@8pF(46');
+define('DB_DATABASE', 'shubhoinfo_oc790');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oclo_');
